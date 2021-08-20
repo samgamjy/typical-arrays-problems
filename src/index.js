4a2b2073dd765dc2612690161280836a676238dd
miklevasilevich <miklevasilevich@gmail.com>
2021-08-20 17:21:24 +0300

    feat: added functions avg / min / max

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+function checkValidArr(array) {
+    
+    return (array !== undefined) && (array.length > 0);
+}
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (!checkValidArr(array)) {
+        return 0;
+      }
+      let min = array[0];
+      for (let i = 1; i < array.length; i += 1) {
+        if (min > array[i]) {
+            min = array[i];
+          }
+      }
+      return min;
+    }
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (!checkValidArr(array)) {
+        return 0;
+      }
+      let max = array[0];
+      for (let i = 1; i < array.length; i += 1) {
+        if (max < array[i]) {
+            max = array[i];
+          }
+      }
+      return max;
+    }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (!checkValidArr(array)) {
+        return 0;
+      }
+      let sum = 0;
+      for (let i = 0; i < array.length; i += 1) {
+        sum += array[i];
+      }
+      return sum / array.length;
+    }
